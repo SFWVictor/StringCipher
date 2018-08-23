49739fea8aaf1b3fce62ff54eef51f06b662b21a
Victor Shenderovich <victor.shenderovich@gmail.com>
2018-08-24 01:51:29 +0300

    Remove redundant using statements and Dispose calls.

--- StringCipher.cs
+++ StringCipher.cs
@@ -32,20 +32,16 @@
                     symmetricKey.Padding = PaddingMode.PKCS7;
                     using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
                     {
-                        using (var memoryStream = new MemoryStream())
+                        var memoryStream = new MemoryStream();
+                        using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
                         {
-                            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                            {
-                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                                cryptoStream.FlushFinalBlock();
-                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
-                                var cipherTextBytes = saltStringBytes;
-                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Convert.ToBase64String(cipherTextBytes);
-                            }
+                            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                            cryptoStream.FlushFinalBlock();
+                            // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+                            var cipherTextBytes = saltStringBytes;
+                            cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                            cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                            return Convert.ToBase64String(cipherTextBytes);
                         }
                     }
                 }
@@ -74,16 +70,12 @@
                     symmetricKey.Padding = PaddingMode.PKCS7;
                     using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
                     {
-                        using (var memoryStream = new MemoryStream(cipherTextBytes))
+                        var memoryStream = new MemoryStream(cipherTextBytes);
+                        using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
                         {
-                            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-                            {
-                                var plainTextBytes = new byte[cipherTextBytes.Length];
-                                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
-                            }
+                            var plainTextBytes = new byte[cipherTextBytes.Length];
+                            var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
                         }
                     }
                 }
